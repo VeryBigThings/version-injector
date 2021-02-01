2a311104ac4a820e0ab436c9d4425007a4b8dca3
Dany Shev <danylo.shevchuk@verybigthings.com>
2021-02-01 12:35:24 +0200

    styles added to file

--- src/VersionViewer.ts
+++ src/VersionViewer.ts
@@ -1,10 +1,10 @@
 import {VARIABLES} from './VARIABLES';
 
 enum Style {
-    botRight = 0,
-    botLeft = 1,
-    topLeft = 2,
-    topRight = 3,
+    botRight = 'botRight',
+    botLeft = 'botLeft',
+    topLeft = 'topLeft',
+    topRight = 'topRight',
 }
 
 export default class VersionViewer {
@@ -24,91 +24,13 @@ export default class VersionViewer {
     }
 
     setStyles() {
-        const style = document.createElement('style');
-        switch (this.style) {
-            case Style.botRight: {
-                style.innerHTML = `
-            .version-panel {
-                position: absolute;
-                right: 5px;
-                bottom: 2px;
+        this.element.classList.add(this.style);
 
-                opacity: 0.1;
-                font-size: 12px;
-                color: #222222;
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'styles.css';
 
-                transition: opacity 0.15s ease-in-out;
-                user-select: none;
-            }
-
-            .version-panel:hover {
-                opacity: 1;
-            }   `;
-                break;
-            }
-
-            case Style.botLeft: {
-                style.innerHTML = `
-            .version-panel {
-                position: absolute;
-                right: 5px;
-                bottom: 2px;
-
-                opacity: 0.1;
-                font-size: 12px;
-                color: #222222;
-
-                transition: opacity 0.15s ease-in-out;
-                user-select: none;
-            }
-
-            .version-panel:hover {
-                opacity: 1;
-            }   `;
-                break;
-            }
-
-            case Style.topLeft: {
-                style.innerHTML = `
-            .version-panel {
-                position: absolute;
-                right: 5px;
-                bottom: 2px;
-
-                opacity: 0.1;
-                font-size: 12px;
-                color: #222222;
-
-                transition: opacity 0.15s ease-in-out;
-                user-select: none;
-            }
-
-            .version-panel:hover {
-                opacity: 1;
-                }`;
-                break;
-            }
-            case Style.topRight: {
-                style.innerHTML = `
-            .version-panel {
-                position: absolute;
-                right: 5px;
-                bottom: 2px;
-
-                opacity: 0.1;
-                font-size: 12px;
-                color: #222222;
-
-                transition: opacity 0.15s ease-in-out;
-                user-select: none;
-            }
-
-            .version-panel:hover {
-                opacity: 1;
-                }`;
-                break;
-            }
-        }
-        document.head.appendChild(style);
+        document.head.appendChild(link);
     }
 }
